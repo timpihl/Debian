ec4c1cadb7977147e4be7f402132adfebc19f1fd
Tim Pihlström <tim@pihl-consulting.se>
2022-04-08 10:32:39 +0200

    Create cloudpanel-uninstall.sh

---
+++ cloudpanel-uninstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+apt purge -y php* mysql* nginx* percona-server-* postfix* redis* memcached* nodejs* clp*
+
+apt autopurge -y
+
+rm -rf /etc/cloudpanel*
+rm -rf /etc/mysql*
+rm -rf /etc/alternatives/my.cnf
+rm -rf /etc/nginx*
+rm -rf /etc/postfix*
+rm -rf /var/mail*
+rm -rf /var/www*
+rm -rf /usr/bin/clpctl
+rm -rf /usr/local/bin/clp_reset_permissions
+rm -rf /home/mysql*
+rm -rf /home/cloudpanel*
+rm -rf /home/clp*
+rm -rf /home/clp-admin*
+
+killall -u clp
+killall -u clp-admin
+
+deluser clp
+deluser clp-admin
+delgroup clp
+delgroup clp-admin
